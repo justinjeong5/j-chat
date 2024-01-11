e445bb0e19a6cbccc7a0e955011b730d15890ffd
justinjeong5 <justin.jeong5@gmail.com>
2024-01-11 20:32:44 +0900

    WIP

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    reactStrictMode: true,
+};
+
+module.exports = {
+    ...nextConfig,
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.externals.push({
+                bufferutil: "bufferutil",
+                "utf-8-validate": "utf-8-validate",
+                "supports-color": "supports-color",
+            });
+        }
+
+        return config;
+    },
+};
