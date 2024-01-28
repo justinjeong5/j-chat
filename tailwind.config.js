4e7850322a48e3a7d7676d639f9b3c6f39c21ac8
justinjeong5 <justin.jeong5@gmail.com>
2024-01-28 18:30:02 +0900

    채팅 스크롤에 음영 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,12 @@ module.exports = {
             animation: {
                 load: "load 1.5s infinite",
             },
+            boxShadow: {
+                "scroll-up": "inset 0 11px 8px -10px rgb(0 0 0 / 0.05)",
+                "scroll-down": "inset 0 -11px 8px -10px rgb(0 0 0 / 0.05)",
+                "scroll-both":
+                    "inset 0 11px 8px -10px rgb(0 0 0 / 0.05), inset 0 -11px 8px -10px rgb(0 0 0 / 0.05)",
+            },
         },
     },
     plugins: [],
