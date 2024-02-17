5cf68ed02b1338407fb8a10b1d0b8f53f347e84b
justinjeong5 <justin.jeong5@gmail.com>
2024-02-17 15:55:21 +0900

    메인 로티 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import AppFrame from "@app/_component/AppFrame";
 import Header from "@app/_component/Header";
-import LottieLayout from "@app/_component/LottieLayout";
 import Menu from "@app/_component/Menu";
 import WithAuth from "@app/_hoc/WithAuth";
 import WithLottie from "@app/_hoc/WithLottie";
@@ -14,12 +13,6 @@ import { TUser } from "@t/user.type";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-import LottieMain from "../../public/lottie/main.json";
-
-function ContentFrame({ children }) {
-    return <div className={cn("m-2")}>{children}</div>;
-}
-
 function Home({ user }: { user: TUser }) {
     const router = useRouter();
     const { lastPage } = usePageRemember();
@@ -28,18 +21,36 @@ function Home({ user }: { user: TUser }) {
         if (lastPage) {
             router.replace(`rooms/${lastPage}`);
         }
-    }, []);
+    }, [lastPage]);
 
     return (
         <AppFrame menu={<Menu user={user} />}>
-            <Header title="첫 페이지">
-                <div>JChat에 오신 것을 환영합니다.</div>
+            <Header title="JChat에 오신 것을 환영합니다.">
+                <div>
+                    JChat은 다양한 사람과 즐거운 이야기를 나누는 대화
+                    플랫폼입니다.
+                </div>
             </Header>
-            <ContentFrame>
-                <LottieLayout>
-                    <Player src={LottieMain} autoplay loop />
-                </LottieLayout>
-            </ContentFrame>
+            <div
+                className={cn(
+                    "flex",
+                    "items-center",
+                    "justify-between",
+                    "h-4/5",
+                    "w-max",
+                    "m-auto",
+                )}
+            >
+                <div>
+                    <Player
+                        className={cn("w-32")}
+                        src="/lottie/main.json"
+                        autoplay
+                        loop
+                    />
+                    <div className={cn("m-4")}>대화를 시작해 보세요.</div>
+                </div>
+            </div>
         </AppFrame>
     );
 }
